dc097a5c525f49dc7e1cf63e2fe4e851c76805b9
Gento <a551.return@gmail.com>
2025-01-23 19:50:21 +0900

    params でのパラメーター取得を非同期で行うように変更

--- app/community/[id]/page.tsx
+++ app/community/[id]/page.tsx
@@ -2,13 +2,15 @@ import BackButton from "@/components/user/BackButton";
 import { fetchCommunityData } from "../../../firebase/user/firestore";
 
 type CommunityPageProps = {
-  params: { id: string };
+  children: React.ReactNode;
+
+  params: Promise<{ id: string }>;
 };
 
 export default async function CommunityPage({ params }: CommunityPageProps) {
   // ドキュメント ID で Firestore データを取得
   const communities = await fetchCommunityData();
-  const community = communities.find((c) => c.id === params.id);
+  const community = communities.find(async (c) => c.id === (await params).id);
 
   if (!community) {
     return <p>コミュニティが見つかりませんでした。</p>;

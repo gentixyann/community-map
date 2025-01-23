ba7eae88134363c5f760103f14ce04fc739ab039
Gento <a551.return@gmail.com>
2025-01-24 02:06:18 +0900

    params を事前に await で解決

--- app/community/[id]/page.tsx
+++ app/community/[id]/page.tsx
@@ -2,13 +2,18 @@ import BackButton from "@/components/user/BackButton";
 import { fetchCommunityData } from "../../../firebase/user/firestore";
 
 type CommunityPageProps = {
-  params: Promise<{ id: string }>;
+  params: Promise<{ id: string }>; // params は Promise で解決される
 };
 
 export default async function CommunityPage({ params }: CommunityPageProps) {
+  // params を事前に await で解決
+  const { id } = await params;
+
   // ドキュメント ID で Firestore データを取得
   const communities = await fetchCommunityData();
-  const community = communities.find(async (c) => c.id === (await params).id);
+
+  // 非同期処理なしで一致するデータを同期的に検索
+  const community = communities.find((c) => c.id === id);
 
   if (!community) {
     return <p>コミュニティが見つかりませんでした。</p>;
